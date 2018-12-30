c16ff966b2344aaf3b621ef1b2d98f9a579bc445
JaiClark <clarkj1222@gmail.com>
2018-12-29 20:12:18 -0500

    Progress as of 12/29/2019

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
   console.log("I'm Working");
+  console.log("Working");
+  console.log("lets");
   
-  var url="https://wind-bow.gomix.me/twitch-api/streams/featured";
+// API key here
+  var id="adv38tiy6van3epbh0hr2q6uhdfkk3";
+
+// use the term /users/(insert name here) for description of streamer or use /streams/<channel ID> for status of streamer(online or offline)
+  var url="https://api.twitch.tv/kraken/users/kinggothalion?client_id=" + id;
 
   $.ajax({
       url: url,
       dataType: "jsonp",
       success: function(data){
-          console.log(data);
+        console.log(data);
+          $(".results").append("<div class='card-body'>" + data.display_name + "</div><br>");
+          $(".results").append("<br><div>" + data._links.self + "</br><br>");
       }
-  })
+  });
 });
