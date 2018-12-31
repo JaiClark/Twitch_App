ee005d1bc2ce2b879e94380152fad70aa08f0f9a
JaiClark <clarkj1222@gmail.com>
2018-12-31 11:55:56 -0500

    Progress as of 12/31/2018 @ 11:55am

--- js/app.js
+++ js/app.js
@@ -1,21 +1,46 @@
 $(document).ready(function() {
   console.log("I'm Working");
-  console.log("Working");
-  console.log("lets");
   
-// API key here
-  var id="adv38tiy6van3epbh0hr2q6uhdfkk3";
 
-// use the term /users/(insert name here) for description of streamer or use /streams/<channel ID> for status of streamer(online or offline)
-  var url="https://api.twitch.tv/kraken/users/kinggothalion?client_id=" + id;
+  // API key here
+  var id = "adv38tiy6van3epbh0hr2q6uhdfkk3";
+
+  // Array of Twitch Streamers
+
+  const streamers = [
+    "freecodecamp",
+    "kinggothalion",
+    "ninja",
+    "fl0m",
+    "JASONR"
+  ];
+
+  // use the term /users/(insert name here) for description of streamer or use /streams/<channel ID> for status of streamer(online or offline)
+  var url = "https://api.twitch.tv/kraken/users/kinggothalion?client_id=" + id;
+
+
+  for (i = 0; i < streamers.length; i++) {
+    console.log(streamers[i]);
+  }
+
+
+
+  function streamFunction() {
+    x = $("online");
+    x.innerHTML = streamers.map(function(itm, i){
+      console.log("https://api.twitch.tv/kraken/users/" + streamers[i] + "?client_id=" + id);
+    });
+  }
+streamFunction();
+
+
+
 
   $.ajax({
-      url: url,
-      dataType: "jsonp",
-      success: function(data){
-        console.log(data);
-          $(".results").append("<div class='card-body'>" + data.display_name + "</div><br>");
-          $(".results").append("<br><div>" + data._links.self + "</br><br>");
-      }
+    url: url,
+    dataType: "jsonp",
+    success: function(data) {
+     
+    }
   });
 });
