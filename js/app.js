baf1fb6d4ce605ff46d980333afdd9eae897e111
JaiClark <clarkj1222@gmail.com>
2019-01-06 21:44:32 -0500

    Progress as of 12/6/2019

--- js/app.js
+++ js/app.js
@@ -5,25 +5,35 @@ $(document).ready(function() {
   var id = "adv38tiy6van3epbh0hr2q6uhdfkk3";
 
   // Array of Twitch Streamers
-
-  var streamers = ["freecodecamp", "kinggothalion", "ninja", "fl0m", "JASONR"];
+  const streamers = ["freecodecamp", "kinggothalion", "ninja", "fl0m", "JASONR"];
 
   // use the term /users/(insert name here) for description of streamer or use /streams/<channel ID> for status of streamer(online or offline)
 
-  for (i = 0; i < streamers.length; i++) {
-    var url =
-      "https://api.twitch.tv/kraken/users/" + streamers[i] + "?client_id=" + id;
+  for (var i = 0; i < streamers.length; i++){
+    var url = "https://api.twitch.tv/kraken/users/" + streamers[i] + "?client_id=" + id;
 
     console.log(url);
   }
 
+  streamer = [];
+  streamer[0] = "freecodecamp";
+  streamer[1] = "kinggothalion";
+  streamer[2] = "ninja";
+  streamer[3] = "fl0m";
+  streamer[4] = "JASONR";
+
+
   $.ajax({
     url: url,
+    // data: DataTransfer,
     dataType: "jsonp",
-    success: function(response) {
-      streamers.map(function(itm, i) {
-        console.log(response);
-      });
+    success: function(data) {
+
+      console.log(data);
+      
+      // streamers.map(function(itm, i) {
+      //   console.log(response);
+      // });
 
     }
   });
